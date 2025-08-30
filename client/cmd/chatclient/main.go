40c844ac642661870b715eb564443f12d881c86a
Avik Mukherjee <avik.nsticti22@gmail.com>
2025-08-30 20:51:44 +0530

    Update the code

--- client/cmd/chatclient/main.go
+++ client/cmd/chatclient/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+	"bufio"
+)
 
 func main() {
-    fmt.Println("Client running...")
+	// Connect to the server
+	conn, err := net.Dial("tcp", "server:8080") // server is the name of the Docker service for the server
+	if err != nil {
+		fmt.Println("Error connecting to the server:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	// Read from user input and send to server
+	fmt.Println("Connected to the server. Type messages to send:")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		_, err := fmt.Fprintf(conn, scanner.Text()+"\n")
+		if err != nil {
+			fmt.Println("Error sending message:", err)
+			return
+		}
+	}
 }
+
--- server/cmd/chatserver/main.go
+++ server/cmd/chatserver/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-    fmt.Println("Server running...")
+	// Listen for incoming connections
+	listen, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error starting the server:", err)
+		os.Exit(1)
+	}
+	defer listen.Close()
+	fmt.Println("Server started on port 8080...")
+
+	// Handle client connections
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleClient(conn) // Handle each client in a new goroutine
+	}
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Client connected:", conn.RemoteAddr())
+
+	// Read messages from the client
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading from client:", err)
+			break
+		}
+		// Print received message from the client
+		fmt.Printf("Message from client: %s\n", string(buffer[:n]))
+	}
 }
